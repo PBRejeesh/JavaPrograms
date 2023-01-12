6757100e87b3d109e33174a0ef1074b1fec6d905
Rejeesh <31933936+PBRejeesh@users.noreply.github.com>
2023-01-12 08:58:01 +0530

    Done code changes
    OutPut
    .........................
    Enter limit:
    20
    The list of first 20 terms in the Fibonacci Sequence is: 
    F0 -> 0
    F1 -> 1
    F2 -> 1
    F3 -> 2
    F4 -> 3
    F5 -> 5
    F6 -> 8
    F7 -> 13
    F8 -> 21
    F9 -> 34
    F10 -> 55
    F11 -> 89
    F12 -> 144
    F13 -> 233
    F14 -> 377
    F15 -> 610
    F16 -> 987
    F17 -> 1597
    F18 -> 2584
    F19 -> 4181
    F20 -> 6765
    ********

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,48 +1,56 @@
 import java.util.Scanner;
 
-class Fibonacci{
+/*
+ * @author : Rejeesh
+ * 
+ * Fibonacci number 
+ * Example output - 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 
+ *
+ **/
 
-    public static void main(String[] args){
+public class Fibonacci {
 
-    Fibonacci fib=new Fibonacci();
-    System.out.println("Fibonacci series ,Author:Rejeesh");
-    
-    int limit=0;
+	public static void main(String[] args) {
 
-    if(args.length==0)
-    {
-    Scanner in=new Scanner(System.in);
-     
-    System.out.println("Enter Fibonacci series length:");
-    limit = in.nextInt();
-    }
-     else
-     limit=Integer.parseInt(args[0]);     
-        
-    System.out.println("Fibonacci series........");
-    
-    for(int i=1;i<=limit;i++){
-            
-     System.out.println(fib.fibonacci(i));   
-     }
-     
-    System.out.println("****End series****");   
-  }
+		Fibonacci fib = new Fibonacci();
+		int limit = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter limit:");
+		limit = in.nextInt();
 
-  public int fibonacci(int num){
+		System.out.println("The list of first " + limit + " terms in the Fibonacci Sequence is: ");
 
-    int f1=1;
-    int f2=1;
-    int temp=0;
+		if (limit >= 0)
+			System.out.println("F0 -> " + 0);
+		else
+			System.err.println("Invalid Input");
 
-    if(num==1 || num ==2)
-     return 1;
-     
-     for(int j=3;j<=num;j++){
-       temp=f2;
-       f2=f1+f2;
-       f1=temp;
-     }
-      return f2;  
-  }
+		for (int i = 1; i <= limit; i++) {
+
+			System.out.println("F" + i + " -> " + fib.fibonacci(i));
+		}
+
+		System.out.println("********");
+	}
+
+	public int fibonacci(int num) {
+
+		int f1 = 1;
+		int f2 = 1;
+		int temp = 0;
+
+		if (num == 1 || num == 2)
+			return 1;
+
+		for (int j = 3; j <= num; j++) {
+			temp = f2;
+			f2 = f1 + f2;
+			f1 = temp;
+		}
+		return f2;
+	}
+
 }
+
+
+
